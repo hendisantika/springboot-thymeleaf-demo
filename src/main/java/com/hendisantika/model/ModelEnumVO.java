fe2a43c8e7b4e92ea2f3bdbfceb0e76dec7bd58a
Hendi Santika <hendisantika@gmail.com>
2021-08-23 07:05:54 +0700

    #5 - Add ModelEnumVO

---
+++ src/main/java/com/hendisantika/model/ModelEnumVO.java
@@ -0,0 +1,19 @@
+package com.hendisantika.model;
+
+import com.hendisantika.Color;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/08/21
+ * Time: 07.05
+ */
+@Data
+public class ModelEnumVO {
+    private String name;
+    private Color color;
+}
