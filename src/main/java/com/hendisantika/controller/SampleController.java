ae06368cf8126e0f4d9fb743a2f949c45e884b71
Hendi Santika <hendisantika@gmail.com>
2021-08-23 07:12:40 +0700

    #11 - Add ex02 in SampleController

--- src/main/java/com/hendisantika/controller/SampleController.java
+++ src/main/java/com/hendisantika/controller/SampleController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,12 +24,24 @@ public class SampleController {
     @GetMapping("/ex01")
     public String ex01(Model model) {
         ProductVO productVO = new ProductVO();
-        productVO.setName("상품A");
+        productVO.setName("Uzumaki Naruto");
         productVO.setPrice(10000);
         productVO.setRegDate(new Date());
 
         model.addAttribute("productVO", productVO);
 
         return "thymeleaf/object_view";
+    }
+
+    @GetMapping("/ex02")
+    public String ex02(Model model) {
+
+        List<ProductVO> productList = new ArrayList<>();
+        IntStream.range(0, 10).forEach(i -> {
+            productList.add(new ProductVO("Goods" + i, i * 1000, new Date()));
+        });
+        model.addAttribute("productList", productList);
+
+        return "thymeleaf/list_view";
     }
 }
