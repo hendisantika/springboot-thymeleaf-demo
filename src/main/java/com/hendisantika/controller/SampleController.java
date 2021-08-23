4fd687471df85392970e7b4d47259516b9feb53f
Hendi Santika <hendisantika@gmail.com>
2021-08-23 07:09:42 +0700

    #9 - Add SampleController

---
+++ src/main/java/com/hendisantika/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.hendisantika.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/08/21
+ * Time: 07.08
+ */
+@Controller
+public class SampleController {
+
+}
